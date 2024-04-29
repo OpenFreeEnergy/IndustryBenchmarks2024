c2cf8e4982bef0718d597a65645ce2a389070e24
IAlibay <IAlibay@users.noreply.github.com>
2024-04-29 10:49:10 +0100

    Update to optionally deal with cofactors

--- utils/input_validation.py
+++ utils/input_validation.py
@@ -10,6 +10,12 @@ from rdkit import Chem
 
 
 def get_settings():
+    """
+    Utility method for getting MDProtocol settings.
+
+    These settings mostly follow defaults but use very short
+    simulation times to avoid being too much of a burden on users' machines.
+    """
     settings = openfe.protocols.openmm_md.plain_md_methods.PlainMDProtocol.default_settings()
     settings.simulation_settings.equilibration_length_nvt = 1 * unit.picosecond
     settings.simulation_settings.equilibration_length = 1 * unit.picosecond
@@ -18,6 +24,19 @@ def get_settings():
 
 
 def run_md(dag):
+    """
+    Run a DAG and check it was ok.
+
+    Parameters
+    ----------
+    dag : openfe.ProtocolDAG
+      A ProtocolDAG to execute.
+
+    Raises
+    ------
+    AssertionError
+      If any of the simulation Units failed.
+    """
     with tempfile.TemporaryDirectory() as tmpdir:
         workdir = pathlib.Path(tmpdir)
         dagres = gufe.protocols.execute_DAG(
@@ -48,26 +67,46 @@ def run_md(dag):
     help="Path to the prepared cofactors SDF file (optional)",
 )
 def run_inputs(pdb, cofactors):
+    """
+    Validate input files by running a short MD simulation
+
+    Parameters
+    ----------
+    pdb : pathlib.Path
+      A Path to a protein PDB file.
+    cofactors : Optional[pathlib.Path]
+      A Path to an SDF file containing the system's cofactors.
+    """
+    # Create the solvent and protein components
     solv = openfe.SolventComponent()
     prot = openfe.ProteinComponent.from_pdb_file(str(pdb))
-    cofactors = [openfe.SmallMoleculeComponent(m) for m in Chem.SDMolSupplier(str(cofactors), removeHs=False)]
 
-    settings = get_settings()
-
-    protocol = PlainMDProtocol(settings=settings)
-
+    # Store there in a components dictionary
     components_dict = {
         'protein': prot,
         'sovlent': solv,
     }
 
-    for cofactor, entry in zip(cofactors, string.ascii_lowercase):
-        components_dict[entry] = cofactor
+    # If we have cofactors, populate them and store them based on
+    # an single letter index (we assume no more than len(alphabet) cofactors)
+    if cofactors is not None:
+        cofactors = [
+            openfe.SmallMoleculeComponent(m)
+            for m in Chem.SDMolSupplier(str(cofactors), removeHs=False)
+        ]
 
+        for cofactor, entry in zip(cofactors, string.ascii_lowercase):
+            components_dict[entry] = cofactor
+
+    # Create the ChemicalSystem
     system = openfe.ChemicalSystem(components_dict)
 
-    dag = protocol.create(stateA=system, stateB=system, mapping=None)
+    # Get the settings and create the protocol
+    settings = get_settings()
+    protocol = PlainMDProtocol(settings=settings)
 
+    # Now create the DAG and run it
+    dag = protocol.create(stateA=system, stateB=system, mapping=None)
     run_md(dag)
 
 
