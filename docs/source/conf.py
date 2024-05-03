fb78371114f346a10a8bb8bafdb9b57044e2de52
IAlibay <IAlibay@users.noreply.github.com>
2024-05-03 12:50:36 +0100

    update project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Industry Benchmark 2024'
+project = 'OpenFE Industry Benchmark 2024'
 copyright = '2024, The OpenFE Development Team'
 author = 'The OpenFE Development Team'
 
