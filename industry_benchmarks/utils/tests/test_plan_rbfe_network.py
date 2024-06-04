eea9bb5907eb3254019938c52158e34c00d6010c
hannahbaumann <hbaumann@uci.edu>
2024-06-04 11:19:51 +0200

    small fix2

--- industry_benchmarks/utils/tests/test_plan_rbfe_network.py
+++ industry_benchmarks/utils/tests/test_plan_rbfe_network.py
@@ -53,12 +53,13 @@ class TestScript:
                     ['--ligands', ligands, '--pdb', protein,
                      '--cofactors', cofactors, '--output', output]
                 )
-                # Read in the LigandNetwork 
+                # Read in the LigandNetwork, check that ligands have partial charges 
                 with open(output / "ligand_network.graphml") as f:
                     graphml = f.read()
                 ligand_network = LigandNetwork.from_graphml(graphml)
                 for edge in ligand_network.edges:
                     assert edge.componentA.to_openff().partial_charges is not None
+                    assert edge.componentB.to_openff().partial_charges is not None
                 assert result.exit_code == 0
                 assert os.path.exists(str(output))
                 # Create a list of all .json files
