bd6c188565de27c67a4d195a08cb6d26cca019ca
Hannah Baumann <43765638+hannahbaumann@users.noreply.github.com>
2024-10-15 10:40:08 +0200

    Update industry_benchmarks/utils/data_gathering.py
    Co-authored-by: Benjamin Ries <benjamin-ries@outlook.com>

--- industry_benchmarks/utils/data_gathering.py
+++ industry_benchmarks/utils/data_gathering.py
@@ -39,7 +39,7 @@ def get_transformation_network_map(
     return blinded_network
 
 
-def get_number_rotatable_bonds(smc):
+def get_number_rotatable_bonds(smc: SmallMoleculeComponent)->int:
     """
     Calculates the number of rotatable bonds in a molecule.
     """
